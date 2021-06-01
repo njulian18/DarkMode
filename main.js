a4813eaf529f3cfe8c9853910cec552fec5d798f
Julian Nuñez <69222010+njulian18@users.noreply.github.com>
2021-06-01 13:13:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let body = document.querySelector(".container")
+let toggle = document.getElementById("toggle")
+
+
+toggle.addEventListener('click', e =>{
+if (toggle.checked) {
+    body.classList.add('dark')
+} else{
+    body.classList.remove('dark')
+}
+
+});
